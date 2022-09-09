6db1dbce58dffa262fb9a80e77a22c131b896b41
Ifeanyi Osi-Okeke <ifeanyi.osi.okeke@gmail.com>
2022-09-09 04:14:35 +0100

    qq

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -3,7 +3,7 @@ import express from "express";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("hello thiss is auth endpoint");
+  res.send("hello this is auth endpoint");
 });
 
 export default router;
